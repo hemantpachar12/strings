086250e653b7bc7858db1f88ce1643398f9344a4
hemantpachar12 <93450820+hemantpachar12@users.noreply.github.com>
2023-04-26 03:01:09 +0530

    Add files via upload

---
+++ string.c
@@ -0,0 +1,12 @@
+// hemant kumar, prn - 21070123032
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() 
+{
+	char name[]="ANUSHA";
+	printf("%s",name);
+	return 0;
+}
